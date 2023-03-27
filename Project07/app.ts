e5f9a75d68e5daedfdfcf4cc1f00e692a66a4d36
grabeTaka <128423774+grabeTaka@users.noreply.github.com>
2023-03-27 19:05:30 -0300

    Add files via upload

---
+++ Project07/app.ts
@@ -0,0 +1,10 @@
+function addReturnNumber(n1: number, n2: number): number {
+    return n1 + n2;
+}
+
+function addReturnVoid(addReturnNumber: number): void {
+    console.log(addReturnNumber);
+}
+
+addReturnNumber(addReturnNumber(1, 1))
+
