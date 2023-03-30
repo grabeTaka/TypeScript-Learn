65b730a97fe2734f176d856973059b634d36f196
grabeTaka <128423774+grabeTaka@users.noreply.github.com>
2023-03-29 23:21:10 -0300

    Add files via upload

---
+++ Project15/src/app.ts
@@ -0,0 +1,10 @@
+const hobbies = ['1', '2', '3', '4', '5', '6'];
+const [hobby1, hooby2, ...remainingHobbies] = hobbies;
+
+const person = {
+    name: 'Gab',
+    age: 3
+}
+
+const { age } = person;
+
